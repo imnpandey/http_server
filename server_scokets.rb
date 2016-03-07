9bce2526730cfc1bda430322b1efa06bd4fc7047
Navneet Pandey <npandey057@gmail.com>
2016-03-07 11:53:42 +0530

    Clean Code

--- server_scokets.rb
+++ server_scokets.rb
@@ -1,18 +1,32 @@
 require "socket"
 
-server = TCPServer.new "localhost", 3000
+class MyServer
+  def initialize
+    port = rand(999) + 3000
+    @server = TCPServer.new "localhost", port
+    puts "Connected on port #{port}"
+    req
+  end
 
-loop do
-  client = server.accept
-  request = client.gets
-  STDERR.puts request
+  def req
+    loop do
+      @client = @server.accept
+      request = @client.gets
+      STDERR.puts request
+      res
+      @client.close
+    end
+  end
 
-  response = "OK\n"
-  client.print "HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/plain\r\n" +
-               "Content-Length: #{response.bytesize}\r\n" +
-               "Connection: close\r\n"
-  client.print "\r\n"
-  client.print response
-  client.close
+  def res
+      response = "OK\n"
+      @client.print "HTTP/1.1 200 OK\r\n" +
+                   "Content-Type: text/plain\r\n" +
+                   "Content-Length: #{response.bytesize}\r\n" +
+                   "Connection: close\r\n"
+      @client.print "\r\n"
+      @client.print response
+  end
 end
+
+MyServer.new
