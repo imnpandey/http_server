7fef9312987f104f95693bfe49c9ef469f50ba80
Navneet Pandey <npandey057@gmail.com>
2016-03-08 12:54:02 +0530

    Serve Dynamic connection

--- server_scokets.rb
+++ server_scokets.rb
@@ -13,19 +13,42 @@ class MyServer
       @client = @server.accept
       request = @client.gets
       STDERR.puts request
-      res
+      request_url = parse_url(request)
+      parse_request(request_url)
       @client.close
     end
   end
 
-  def res
-      response = "OK\n"
-      @client.print "HTTP/1.1 200 OK\r\n" +
-                   "Content-Type: text/plain\r\n" +
-                   "Content-Length: #{response.bytesize}\r\n" +
-                   "Connection: close\r\n"
+  def res(response, status=200)
+      message = "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: #{response.bytesize}\r\n" +
+                "Connection: close\r\n"
+      unless status == 200
+        message = "HTTP/1.1 404 Not Found\r\n" +
+                  "Connection: close\r\n"
+      end
+      @client.print message
       @client.print "\r\n"
       @client.print response
+  end
+
+  def parse_url(request)
+    request_uri  = request.split(" ")[1]
+  end
+
+  def parse_request(request_url)
+    case request_url
+    when "/home"
+      res("You are on home")
+    when "/about"
+      res("You are on about")
+    when "/"
+      res("welcome, root")
+    else
+      res("Not Found", 404)
+    end
+
   end
 end
 
