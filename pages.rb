2b9f2b747c75a66cc1d5c7f73e1b4314b99eec34
Navneet Pandey <npandey057@gmail.com>
2016-03-09 12:22:54 +0530

    add DB to store data

--- pages.rb
+++ pages.rb
@@ -1,10 +1,12 @@
+require 'sqlite3'
+
 module Pages
   def parse_request(method, request_url)
     @root_path = "public/"
     file = "index"
     file = get_file_name(request_url) unless request_url == "/"
     if method == "POST"
-      write_file
+      store_DB
     elsif method == "GET" && file_exists?(file)
       message = serve_file(file)
       response message
@@ -29,10 +31,24 @@ module Pages
     text.read
   end
 
-  def write_file
-    referer = @headers["Referer"].split("/")[-1]
-    message = @request.read(@headers["Content-Length"].to_i)
-    IO.write("#{@root_path}#{referer}.txt", message)
-    response message
+  def parse_post_data
+    message = @request.read(@headers["Content-Length"].to_i).split("&")
+    hash = {}
+    message.each do |data|
+      value = data.split("=")
+      hash[value[0]] = value[1].strip
+    end
+    hash
+  end
+
+  def connect_DB
+    @database = SQLite3::Database.open( "server.db" )
+    @database.execute( "CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY, name TEXT, email TEXT, comments TEXT);" )
+  end
+
+  def store_DB
+    connect_DB
+    data_hash = parse_post_data
+    @database.execute("INSERT INTO comments (name, email, comments) VALUES ('#{data_hash['name']}', '#{data_hash['email']}', '#{data_hash['comments']}');")
   end
 end
